db7f8472c93ae21be97a0373311ef487ae57f692
Alisson Youssf <youssfbr@gmail.com>
2022-09-20 22:20:36 -0300

    Create Cliente

--- agencia-app/src/main/java/com/github/youssfbr/cliente/ClienteResource.java
+++ agencia-app/src/main/java/com/github/youssfbr/cliente/ClienteResource.java
@@ -1,10 +1,9 @@
 package com.github.youssfbr.cliente;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/cliente")
@@ -21,6 +20,27 @@ public class ClienteResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Cliente findById(@QueryParam("id") long id) {
         return Cliente.findById(id);
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response newCliente(Cliente cliente) {
+        cliente.id = null;
+        cliente.persist();
+
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(cliente)
+                .build();
+    }
+
+    @DELETE
+    @Transactional
+    @Path("deleteById")
+    public void deleteById(long id) {
+        Cliente.deleteById(id);
     }
 
 }
